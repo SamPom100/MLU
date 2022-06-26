3c46146c658fbb311002111f91d9be0ecb1a1a07
Sam Pomerantz <sam@sampomerantz.me>
2022-06-26 17:14:49 -0500

    added gitignore, removed some params

--- ml_stack.py
+++ ml_stack.py
@@ -1,5 +1,6 @@
 import re, os, pandas as pd, autogluon.core as ag
 from autogluon.tabular import TabularPredictor, TabularDataset
+from autogluon.text import TextPredictor
 
 def first_num(in_val):
     num_string = in_val.split(" ")[0]
@@ -29,8 +30,8 @@ def drop_features(in_feat):
 
 
 
-df_train = TabularDataset(data="../data/training.csv")
-df_test = TabularDataset(data="../data/mlu-leaderboard-test.csv")
+df_train = TabularDataset(data="./data/training.csv")
+df_test = TabularDataset(data="./data/mlu-leaderboard-test.csv")
 
 #CLEAN TRAIN DATA
 full_feateng = df_train.copy()
@@ -43,62 +44,11 @@ full_feateng.drop(['Edition', 'Ratings', 'Reviews'], axis=1, inplace=True)
 
 
 
-
-
-
-# Set Neural Net options
-# Specifies non-default hyperparameter values for neural network models
-nn_options = {
-    # number of training epochs (controls training time of NN models)
-    "num_epochs": 50,
-    # learning rate used in training (real-valued hyperparameter searched on log-scale)
-    "learning_rate": ag.space.Real(1e-4, 1e-2, default=5e-4, log=True),
-    # activation function used in NN (categorical hyperparameter, default = first entry)
-    "activation": ag.space.Categorical("relu", "softrelu", "tanh"),
-    # each choice for categorical hyperparameter 'layers' corresponds to list of sizes for each NN layer to use
-    "layers": ag.space.Categorical([100], [1000], [200, 100], [300, 200, 100]),
-    # dropout probability (real-valued hyperparameter)
-    "dropout_prob": ag.space.Real(0.0, 0.5, default=0.1),
-}
-
-# Set GBM options
-# Specifies non-default hyperparameter values for lightGBM gradient boosted trees
-gbm_options = {
-    # number of boosting rounds (controls training time of GBM models)
-    "num_boost_round": 500,
-    # number of leaves in trees (integer hyperparameter)
-    "num_leaves": ag.space.Int(lower=26, upper=66, default=36),
-}
-
-# Add both NN and GBM options into a hyperparameter dictionary
-# hyperparameters of each model type
-# When these keys are missing from the hyperparameters dict, no models of that type are trained
-hyperparameters = {
-    "GBM": gbm_options,
-    "NN": nn_options,
-}
-
-# To tune hyperparameters using Bayesian optimization to find best combination of params
-search_strategy = "auto"
-
-# Number of trials for hyperparameters
-num_trials = 5
-
-# HPO is not performed unless hyperparameter_tune_kwargs is specified
-hyperparameter_tune_kwargs = {
-    "num_trials": num_trials,
-    "scheduler": "local",
-    "searcher": search_strategy,
-}
-
-
-
 #BUILD MODEL
-predictor = TabularPredictor(label="Price").fit(
+predictor = TextPredictor(label="Price").fit(
     full_feateng,
-    auto_stack=True,
+    presets='best_quality',
 )
-
 
 
 
@@ -126,7 +76,7 @@ prediction = predictor.predict(test_data_feateng)
 submission = df_test[["ID"]].copy(deep=True)
 submission["Price"] = prediction
 submission.to_csv(
-    "../data/predictions/autoStack1.csv",
+    "./data/predictions/autoStack1.csv",
     index=False,
 )
 
